930abe76a6ce8447b79624e1b34ab306d00c1702
Luan <18734184215@163.com>
2020-12-14 16:58:23 +0800

    实现顶部的导航的自动滚动

--- HiUI/hi-ui/src/main/java/com/example/hi_ui/ui/tab/top/HiTabTopLayout.java
+++ HiUI/hi-ui/src/main/java/com/example/hi_ui/ui/tab/top/HiTabTopLayout.java
@@ -1,6 +1,7 @@
 package com.example.hi_ui.ui.tab.top;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 
+import com.example.hi_library.log.utils.HiDisplayUtil;
 import com.example.hi_ui.ui.tab.bottom.HiTabBottom;
 import com.example.hi_ui.ui.tab.bottom.HiTabBottomInfo;
 import com.example.hi_ui.ui.tab.common.IHiTabLayout;
@@ -26,17 +28,18 @@ public class HiTabTopLayout extends HorizontalScrollView implements IHiTabLayout
     private List<HiTabTopInfo<?>> infoList;
 
     public HiTabTopLayout(Context context) {
-        super(context,null);
+        super(context, null);
     }
 
     public HiTabTopLayout(Context context, AttributeSet attrs) {
-        super(context, attrs,0);
+        super(context, attrs, 0);
     }
 
     public HiTabTopLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setVerticalScrollBarEnabled(false);
     }
+
     @Override
     public void addTabSelectedChangeListener(OnTabSelectedListener<HiTabTopInfo<?>> listener) {
         tabSelectedListeners.add(listener);
@@ -68,7 +71,89 @@ public class HiTabTopLayout extends HorizontalScrollView implements IHiTabLayout
             listener.onTabSelectedChange(infoList.indexOf(nextInfo), selectedInfo, nextInfo);
         }
         this.selectedInfo = nextInfo;
+        autoScroll(nextInfo);
     }
+
+    /***
+     * 自动滚动，实现点击的位置能够自动滚动以展示前后2个
+     * @param nextInfo
+     */
+    private void autoScroll(HiTabTopInfo<?> nextInfo) {
+        HiTabTop tabTop = findTab(nextInfo);
+        if (tabTop == null) return;
+        int index = infoList.indexOf(nextInfo);
+        int[] loc = new int[2];
+        //获取view在window中的坐标
+        tabTop.getLocationInWindow(loc);
+        int scrollWidth;
+        if (tabWith == 0) {
+            tabWith = tabTop.getWidth();
+        }
+        // 判断点击了屏幕左侧还是右侧
+        if ((loc[0] + tabTop.getWidth() / 2) > HiDisplayUtil.getDisplayWidthInPx(getContext()) / 2) {
+            scrollWidth = rangeScrollWidth(index, 2);
+        } else {
+            scrollWidth = rangeScrollWidth(index, -2);
+        }
+        scrollTo(getScrollX() + scrollWidth, 0);
+    }
+
+    /***
+     * 获取可滑动的范围
+     * @param index  从第几个开始
+     * @param range  向前向后的范围
+     * @return 可滚动的范围
+     */
+    private int rangeScrollWidth(int index, int range) {
+        int scrollWidth = 0;
+        for (int i = 0; i <= Math.abs(range); i++) {
+            int next;
+            if (range < 0) {
+                next = range + i + index;
+            } else {
+                next = range - i + index;
+            }
+            if (next >= 0 && next < infoList.size()) {
+                if (range < 0) {
+                    scrollWidth -= scrollWidth(next, false);
+                } else {
+                    scrollWidth += scrollWidth(next, true);
+                }
+            }
+        }
+        return scrollWidth;
+    }
+
+
+    /***
+     * 指定位置的控件可滚动的距离
+     * @param index   指定位置的控件
+     * @param toRight   是否点击了屏幕右侧
+     * @return 可滚动的距离
+     */
+    private int scrollWidth(int index, boolean toRight) {
+        HiTabTop target = findTab(infoList.get(index));
+        if (target == null) return 0;
+        Rect rect = new Rect();
+        //判断视图是不是可见 将视图的可见部分 映射到一个rect对象中
+        target.getLocalVisibleRect(rect);
+        if (toRight) {
+            if (rect.right > tabWith) {
+                return tabWith;
+            } else {
+                return tabWith - rect.right;
+            }
+        } else {
+            if (rect.left <= -tabWith) {
+                return tabWith;
+            } else if (rect.left > 0) {
+                return rect.left;
+            }
+            return 0;
+        }
+    }
+
+    int tabWith;
 
     @Override
     public void defaultSelected(@NonNull HiTabTopInfo<?> defaultInfo) {
