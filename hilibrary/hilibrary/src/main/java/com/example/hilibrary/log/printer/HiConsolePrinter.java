270f49d2c118a05c1094d6b8b8055cb95c0a09d0
Luan <18734184215@163.com>
2020-12-14 10:20:08 +0800

    日志打印框架的提交

---
+++ hilibrary/hilibrary/src/main/java/com/example/hilibrary/log/printer/HiConsolePrinter.java
@@ -0,0 +1,29 @@
+package com.example.hilibrary.log.printer;
+
+ import android.util.Log;
+
+import com.example.hilibrary.log.HiLogConfig;
+
+import org.jetbrains.annotations.NotNull;
+
+import static com.example.hilibrary.log.HiLogConfig.MAX_LEN;
+
+public class HiConsolePrinter implements HiLogPrinter {
+    @Override
+    public void print(@NotNull HiLogConfig config, int level, String tag, @NotNull String printString) {
+        int len = printString.length();
+        int countOfSub = len / MAX_LEN;
+        if (countOfSub > 0) {
+            int index = 0;
+            for (int i = 0; i < countOfSub; i++) {
+                Log.println(level, tag, printString.substring(index, index + MAX_LEN));
+                index += MAX_LEN;
+            }
+            if (index != len) {
+                Log.println(level, tag, printString.substring(index, len));
+            }
+        }else{
+            Log.println(level,tag,printString);
+        }
+    }
+}
